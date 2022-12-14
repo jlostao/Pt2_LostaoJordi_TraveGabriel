c55c76c2d21b8d83bc001549f56440cd67d6dea1
jordi <jorlosnav@gmail.com>
2022-12-14 22:28:43 +0100

    Added ex 2

--- Ex 1,2,4.py
+++ Ex 1,2,4.py
@@ -1,2 +1,9 @@
 if sel == 1:
     frase = input("Dame una frase: ")
+if sel == 2:
+    vocales = "aeiou"
+    newfrase = ""
+    for letter in frase:
+        if letter.lower() not in vocales:
+            newfrase += letter
+    frase = newfrase
