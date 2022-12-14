2a60d1b0d80be2a3a899c3c95623ea5638a531aa
gtrave <gabrieltravebiel@gmail.com>
2022-12-14 21:08:32 +0100

    ex3

--- ex_3_5.py
+++ ex_3_5.py
@@ -1,8 +1,16 @@
 
-frase = input("Dame una frase: ")
 vocales = ["a", "e", "i", "o", "u"]
 nueva = ""
 for i in frase:
     if i in vocales:
         nueva = nueva + i
 print(nueva)
+
+frase=input("dame una frase: ")
+frase = frase.split(" ")
+for i in range(len(frase)):
+    a = 1
+    for j in range(0, len(frase) - (a)):
+
+        a += 1
+print(frase)
