5b647b8ec9a703cb0535c7e84fa6bedfc4e7c420
jlostao <119519726+jlostao@users.noreply.github.com>
2022-12-14 22:43:10 +0100

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -4,5 +4,19 @@ menu = "1. Introdueix una frase \n2. Elimina les vocals \n3. Elimina les consona
 
 while True:
     sel = int(input(menu))
+    if sel == 1:
+       frase = input("Dame una frase: ")
+    if sel == 2:
+       vocales = "aeiou"
+       newfrase = ""
+    for letter in frase:
+       if letter.lower() not in vocales:
+            newfrase += letter
+       frase = newfrase
+    if sel == 4:
+       newfrase = ""
+       for i in range(len(frase) - 1, -1, -1):
+            newfrase += frase[i]
+       print(newfrase)
     if sel == 6:
         break
