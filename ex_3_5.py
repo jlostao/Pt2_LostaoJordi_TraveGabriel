c3c425eae77228ac3faa87cc08c9ec7a672f323f
gtrave <gabrieltravebiel@gmail.com>
2022-12-14 21:09:26 +0100

    ex completo el anterior es una a medias

--- ex_3_5.py
+++ ex_3_5.py
@@ -11,6 +11,7 @@ frase = frase.split(" ")
 for i in range(len(frase)):
     a = 1
     for j in range(0, len(frase) - (a)):
-
+        if frase[j][1].lower() > frase[a][1].lower():
+            frase[j], frase[a] = frase[a], frase[j]
         a += 1
 print(frase)
