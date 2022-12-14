af217a23d5168f11c9546cd6b770980e477139b1
jordi <jorlosnav@gmail.com>
2022-12-14 22:28:52 +0100

    Added ex 2

--- Ex 1,2,4.py
+++ Ex 1,2,4.py
@@ -7,3 +7,8 @@ if sel == 2:
         if letter.lower() not in vocales:
             newfrase += letter
     frase = newfrase
+if sel == 4:
+    newfrase = ""
+    for i in range(len(frase) - 1, -1, -1):
+        newfrase += frase[i]
+    print(newfrase)
